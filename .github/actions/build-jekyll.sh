4063d7585730adec5224a25e2f5bb955f1920ff6
elia-b <elia.boscaini@gmail.com>
2023-03-11 12:04:22 +0100

    modify build

--- .github/actions/build-jekyll.sh
+++ .github/actions/build-jekyll.sh
@@ -1,9 +1,9 @@
-apt-get update 
-apt-get install -y ruby-full build-essential zlib1g-dev 
+sudo apt-get update 
+sudo apt-get install -y ruby-full build-essential zlib1g-dev 
 
-export GEM_HOME="$HOME/gems" 
-xport PATH="$HOME/gems/bin:$PATH"
+sudo export GEM_HOME="$HOME/gems" 
+sudo export PATH="$HOME/gems/bin:$PATH"
 
-RUN gem install jekyll bundler
+gem install jekyll bundler
 
 jekyll build --source . --destination ./_site
